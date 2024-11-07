aa0f9468b880c86885edc2a772a025e605c0884c
Can <hasancan.net.tr@gmail.com>
2024-11-07 18:38:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ data = pd.read_csv("data.csv", delimiter=";")
 st.title("Student Outcome Analysis")
 st.write("This app allows you to explore, classify, and compare data on student outcomes.")
 
-# Claim
+# Notice
 st.sidebar.markdown("### Details")
 st.sidebar.markdown("""
 This application developed for Business Analytics class on RTU by Hasan Can, Burak Caka and Vepa Tuliyev
